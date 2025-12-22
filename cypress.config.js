6d0dee066e2f9278d36a64898a7752b94a108c7d
Andre Santos <euandremas@gmail.com>
2025-12-22 02:09:25 -0300

    Configura Cypress para executar testes apontando para o ambiente de produção

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://euandremas.github.io/hamburgueria-webapp",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // eventos do Cypress (não utilizados neste projeto)
     },
   },
 });
